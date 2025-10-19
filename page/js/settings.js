f41dac6f553d6ff7a2e367a632f1a2861340fc9a
Hei-Lima <heitor.lipeixoto@gmail.com>
2025-10-19 20:16:20 -0300

    general refactoring

--- page/js/settings.js
+++ page/js/settings.js
@@ -13,175 +13,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
     cancelBtn.addEventListener("click", function () {
         settingsModal.close();
-        applySettings(); // Changed from change()
+        applySettings(); 
     });
 
     saveBtn.addEventListener("click", function () {
-        saveSettings();
         settingsModal.close();
+        saveSettings();
     });
 
-    // Other initializations
-    applySettings(); // Changed from change()
+    applySettings(); 
     initializeRangeInputs();
 });
-
-// --- Refactored Title Function ---
-function applyTitle(selectedTitle = localStorage.getItem("selectedTitle") || "Default") {
-    const titleDiv = document.getElementById("title");
-    while (titleDiv.firstChild) {
-        titleDiv.removeChild(titleDiv.firstChild);
-    }
-
-    if (selectedTitle !== "Default") {
-        const img = document.createElement("img");
-        img.src = `page/titles/${selectedTitle}.svg`;
-        img.alt = "Hackerfeed";
-        img.className = "w-96 md:w-120";
-        titleDiv.appendChild(img);
-    } else {
-        const h1 = document.createElement("h1");
-        h1.className = "text-6xl md:text-8xl font-bold text-primary drop-shadow-md";
-        h1.textContent = "hackerfeed";
-        titleDiv.appendChild(h1);
-    }
-    document.getElementById("titleSelect").value = selectedTitle;
-}
 
 function applyFont(selectedFont = localStorage.getItem("selectedFont") || "Inter") {
     document.documentElement.style.setProperty("--font-display", `"${selectedFont}", "sans-serif"`);
     document.getElementById("fontSelect").value = selectedFont;
 }
 
-function applyGlass(navGlass = localStorage.getItem("navGlass") === "true", cardGlass = localStorage.getItem("cardGlass") === "true") {
-    document.getElementById("frutigerStyleNav").media = navGlass ? "all" : "none";
-    document.getElementById("frutigerStyleCard").media = cardGlass ? "all" : "none";
-}
-
-function applyNavbar(ghostNav = localStorage.getItem("ghostNav") === "true") {
-    const navbar = document.getElementById("navbar");
-    ghostNav ? navbar.classList.remove("bg-base-200") : navbar.classList.add("bg-base-200");
-}
-
-function applyBackground(selectedBackground = localStorage.getItem("selectedBackground")) {
-    if (selectedBackground) {
-        const gifs = ["Matrix", "Genesis"];
-        const images = ["Glass", "Frutiger"]
-        let backgroundUrl = '';
-
-        if (gifs.includes(selectedBackground)) {
-            backgroundUrl = `/page/background/${selectedBackground}.gif`;
-        } else if (selectedBackground === "Dark Gradient") {
-            document.body.style.backgroundImage = "linear-gradient(to top, #0F2027, #203A43, #2C5364)";
-            return;
-        } else if (selectedBackground === "Light Gradient") {
-            document.body.style.backgroundImage = "linear-gradient(to top, #e0eafc, #cfdef3)";
-            return;
-        } else if (selectedBackground === "Custom Gradient") {
-            const customGradient = JSON.parse(localStorage.getItem("customGradients"));
-            document.body.style.backgroundImage = `linear-gradient(to top, ${customGradient[1]}, ${customGradient[0]})`;
-            return;
-        } else if (selectedBackground === "Default") {
-            document.body.style.backgroundImage = null;
-            return;
-        } else if (images.includes(selectedBackground)) {
-            backgroundUrl = `/page/background/${selectedBackground}.webp`;
-        }
-
-        document.body.style.backgroundImage = `url('${backgroundUrl}')`;
-        document.body.style.backgroundAttachment = "fixed";
-        document.body.style.backgroundPosition = "center";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-    } else {
-        document.body.style.backgroundImage = "";
-    }
-}
-
-function applyBlurDarken(blur = localStorage.getItem("blur") ?? 0, darken = localStorage.getItem("darken") ?? 0) {
-    document.documentElement.style.setProperty("--blur-effect", `blur(${blur}px)`);
-    document.documentElement.style.setProperty("--darken-overlay", `rgba(0, 0, 0, ${darken / 100})`);
-    document.getElementById("blurValue").textContent = `${blur}px`;
-    document.getElementById("darkenValue").textContent = `${darken}%`;
-}
-
 function updatePlaceholders() {
     const settings = {
         saveTime: localStorage.getItem("saveTime") || 15,
-        navGlass: localStorage.getItem("navGlass"),
-        cardGlass: localStorage.getItem("cardGlass"),
-        blur: localStorage.getItem("blur") || 0,
-        darken: localStorage.getItem("darken") || 0,
-        title: localStorage.getItem("selectedTitle") || "Default",
         font: localStorage.getItem("selectedFont") || "Inter",
-        background: localStorage.getItem("selectedBackground") || "Default",
-        customGradient: JSON.parse(localStorage.getItem("customGradients")) || ["#000000", "#000000"],
         fetchLimit: localStorage.getItem("fetchLimit") || 21
     };
 
-    document.getElementById("titleSelect").value = settings.title;
     document.getElementById("fontSelect").value = settings.font;
     document.getElementById("fetchTimeInput").placeholder = `Current: ${settings.saveTime}`;
-    document.getElementById("blurRange").value = settings.blur;
-    document.getElementById("darkenRange").value = settings.darken;
-    document.getElementById("ghostNavCheck").checked = localStorage.getItem("ghostNav") === "true";
-    document.getElementById("blurValue").textContent = `${settings.blur}px`;
-    document.getElementById("darkenValue").textContent = `${settings.darken}%`;
     document.getElementById("fetchTimeInput").value = settings.saveTime;
-    document.getElementById("navbarGlassCheck").checked = settings.navGlass === null ? false : settings.navGlass === "true";
-    document.getElementById("cardGlassCheck").checked = settings.cardGlass === null ? false : settings.cardGlass === "true";
-    document.getElementById("backgroundSelect").value = settings.background || "Default";
     document.getElementById("fetchLimitInput").value = settings.fetchLimit;
-    document.getElementById("gradient1").value = settings.customGradient[0];
-    console.log(settings.customGradient[0]);
-    document.getElementById("gradient1Hex").value = settings.customGradient[0];
-    document.getElementById("gradient2Hex").value = settings.customGradient[1];
-    console.log(document.getElementById("gradient1").value);
-    document.getElementById("gradient2").value = settings.customGradient[1];
 }
 
 function initializeRangeInputs() {
-    const blurRange = document.getElementById("blurRange");
-    const darkenRange = document.getElementById("darkenRange");
     const fontSelect = document.getElementById("fontSelect");
-    const backgroundSelect = document.getElementById("backgroundSelect");
-    const navbarGlassCheck = document.getElementById("navbarGlassCheck");
-    const cardGlassCheck = document.getElementById("cardGlassCheck");
     const fetchTimeInput = document.getElementById("fetchTimeInput");
-    const ghostNavCheck = document.getElementById("ghostNavCheck");
-    const titleSelect = document.getElementById("titleSelect");
     const fetchLimitInput = document.getElementById("fetchLimitInput");
-
-    blurRange.addEventListener("input", function () {
-        applyBlurDarken(this.value, document.getElementById("darkenRange").value);
-    });
-
-    darkenRange.addEventListener("input", function () {
-        applyBlurDarken(document.getElementById("blurRange").value, this.value);
-    });
 
     fontSelect.addEventListener("change", function () {
         applyFont(this.value);
-    });
-
-    titleSelect.addEventListener("change", function () {
-        applyTitle(this.value);
-    });
-
-    backgroundSelect.addEventListener("change", function () {
-        applyBackground(this.value);
-    });
-
-    navbarGlassCheck.addEventListener("change", function () {
-        applyGlass(this.checked, document.getElementById("cardGlassCheck").checked);
-    });
-
-    cardGlassCheck.addEventListener("change", function () {
-        applyGlass(document.getElementById("navbarGlassCheck").checked, this.checked);
-    });
-
-    ghostNavCheck.addEventListener("change", function () {
-        applyNavbar(this.checked);
     });
 
     fetchTimeInput.addEventListener("input", function () {
@@ -197,10 +65,6 @@ function getFontValue() {
     return document.getElementById("fontSelect").value;
 }
 
-function getTitleValue() {
-    return document.getElementById("titleSelect").value;
-}
-
 function getFetchLimitValue() {
     return document.getElementById("fetchLimitInput").value;
 }
@@ -209,94 +73,15 @@ function getFetchTimeValue() {
     return document.getElementById("fetchTimeInput").value;
 }
 
-function getGlassValues() {
-    return {
-        navGlass: document.getElementById("navbarGlassCheck").checked,
-        cardGlass: document.getElementById("cardGlassCheck").checked
-    };
-}
-
-function getNavbarValue() {
-    return document.getElementById("ghostNavCheck").checked;
-}
-
-function getBackgroundValue() {
-    return document.getElementById("backgroundSelect").value;
-}
-
-function getBlurDarkenValues() {
-    return {
-        blur: document.getElementById("blurRange").value,
-        darken: document.getElementById("darkenRange").value
-    };
-}
-
 function saveSettings() {
     localStorage.setItem("selectedFont", getFontValue());
-    localStorage.setItem("selectedTitle", getTitleValue());
     localStorage.setItem("saveTime", getFetchTimeValue());
     localStorage.setItem("fetchLimit", getFetchLimitValue());
-
-    const customGradient1 = document.getElementById("gradient1").value;
-    const customGradient2 = document.getElementById("gradient2").value;
-    localStorage.setItem("customGradients", JSON.stringify([customGradient1, customGradient2]));
-
-    const glassValues = getGlassValues();
-    localStorage.setItem("navGlass", glassValues.navGlass);
-    localStorage.setItem("cardGlass", glassValues.cardGlass);
-
-    localStorage.setItem("selectedBackground", getBackgroundValue());
-
-    const blurDarkenValues = getBlurDarkenValues();
-    localStorage.setItem("blur", blurDarkenValues.blur);
-    localStorage.setItem("darken", blurDarkenValues.darken);
-
-    localStorage.setItem("ghostNav", getNavbarValue());
 
     applySettings();
 }
 
-function initializeColorPickers() {
-    const gradient1 = document.getElementById("gradient1");
-    const gradient1Hex = document.getElementById("gradient1Hex");
-    const gradient2 = document.getElementById("gradient2");
-    const gradient2Hex = document.getElementById("gradient2Hex");
-
-    // Synchronize Gradient 1
-    gradient1.addEventListener("input", () => {
-        gradient1Hex.value = gradient1.value;
-    });
-
-    gradient1Hex.addEventListener("input", () => {
-        if (/^#[0-9A-Fa-f]{6}$/.test(gradient1Hex.value)) {
-            gradient1.value = gradient1Hex.value;
-        }
-    });
-
-    // Synchronize Gradient 2
-    gradient2.addEventListener("input", () => {
-        gradient2Hex.value = gradient2.value;
-    });
-
-    gradient2Hex.addEventListener("input", () => {
-        if (/^#[0-9A-Fa-f]{6}$/.test(gradient2Hex.value)) {
-            gradient2.value = gradient2Hex.value;
-        }
-    });
-}
-
-// Call the function during initialization
-document.addEventListener("DOMContentLoaded", function () {
-    initializeColorPickers();
-});
-
 function applySettings() {
-    applyGlass();
-    applyBackground();
     applyFont();
-    applyTitle();
     updatePlaceholders();
-    applyBlurDarken();
-    applyNavbar();
-    initializeColorPickers();
 }
